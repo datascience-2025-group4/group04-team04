f967dda73c587be501a8dea25fde78d75985b322
MarcelBuss <marcel_buss@gmx.net>
2025-05-19 18:13:54 +0200

    funktion Notebook_vorbereiten repariert

--- common/functions.py
+++ common/functions.py
@@ -4,7 +4,7 @@ def Notebook_vorbereiten():
     import numpy as np
     import matplotlib.pyplot as plt
     import seaborn as sns
-    ab_ag_raw = pd.read_csv('data/ab_ag.tsv', sep='\t'),
-    columns = pd.read_csv('data/columns.tsv', sep='\t'),
+    ab_ag_raw = pd.read_csv('data/ab_ag.tsv', sep='\t')
+    columns = pd.read_csv('data/columns.tsv', sep='\t')
     uniprot_data = pd.read_csv('data/uniprot_data.tsv', sep='\t')
     return ab_ag_raw, columns, uniprot_data
